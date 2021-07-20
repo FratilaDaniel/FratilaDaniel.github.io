bc8675d7589f61cd441f335f485567a4d2a3d043
FratilaDaniel <danfratila1997@gmail.com>
2021-07-20 18:24:32 +0300

    add education section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import LanguagesSection from './components/LanguagesSection';
 import SectionContainer from './components/SectionContainer';
+import EducationSection from './components/EducationSection';
 
 const SECTIONS = [{
     title: "Languages",
     content: <LanguagesSection/> 
-}];
+},{
+    title: "Education",
+    content: <EducationSection/> 
+},
+];
 
 function App() {
     return (
---
+++ src/components/EducationSection.jsx
@@ -0,0 +1,13 @@
+
+function EducationSection(){
+    return (
+        <div className="education-container">  
+                <img src="/SVG/sigla_utcluj_white.svg" alt="Technical University of Cluj Napoca"/>
+                <p className="faculty-container">Computer Science</p>
+                <p className="diploma-container">Engineer Diploma</p>
+                <p className="year-container">2016 - 2020</p>
+        </div>
+    );
+}
+
+export default EducationSection;
