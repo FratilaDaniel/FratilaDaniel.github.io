90bda2afcff5ccc400e7b09ce036b33212acf1b9
FratilaDaniel <danfratila1997@gmail.com>
2021-08-10 13:38:12 +0300

    small visual improvements for projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -33,15 +33,20 @@ function ProjectsSection(){
                 return (
                     <div className="project-container" key={index}>
                         <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <p>Technologies used: <strong>{project.technologies}</strong></p>
-                        <p>View this application deployed on <a href={project.herokuLink}>Heroku</a></p>
-                        <p>View this application's code on <a href={project.githubLink}>Github</a></p>
+                        <div className={index % 2 ? "project-container-content" : "project-container-content-reverse"}>
+                            <img src="/technologies/c.png" alt=""/>
+                            <div>
+                                <p>{project.description}</p>
+                                <p>Technologies used: <strong>{project.technologies}</strong></p>
+                                <p>View this application deployed on <a href={project.herokuLink}>Heroku</a></p>
+                                <p>View this application's code on <a href={project.githubLink}>Github</a></p>
+                            </div>
+                        </div>
+
+                        
                     </div>
                 );
             })}
-            {/* <img className="bkg-img-1" src="/SVG/BackgroundElement_01.svg" alt=""/> */}
-            {/* <img className="bkg-img-2" src="/SVG/BackgroundElement_02.svg" alt=""/> */}
         </div>
     );
 }
