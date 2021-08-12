716c857306047af844394a42476bd8360c4ee0ea
FratilaDaniel <danfratila1997@gmail.com>
2021-08-12 14:40:52 +0300

    header decorators improved, about me section header changed to normal size

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -2,7 +2,7 @@
 function AboutMeSection(){
     return (
         <section id="about-me-section">
-                <h3 className="about-me-title">About me</h3>
+                <h2 className="about-me-title">About me</h2>
                 <p className="about-me-p">Hello! I am web developer. Pls hire me. <a href="#contact">Contact me now!</a></p>    
         </section>
     );
